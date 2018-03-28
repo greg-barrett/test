3cbf1e9939740d5c44d9a3d93a3b1b3aedf8b278
Gregory Barrett <gbarrett15@gmail.com>
2018-03-28 16:57:10 +0200

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,18 @@ if ENV["RAILS_LOG_TO_STDOUT"].present?
   logger.formatter = config.log_formatter
   config.logger = ActiveSupport::TaggedLogging.new(logger)
 end
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'immense-tundra-82363.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 end
